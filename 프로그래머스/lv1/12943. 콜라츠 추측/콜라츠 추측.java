a7e3cd428c0e4445fce9202c5434bd3ec232c08b
lodalee <131640279+lodalee@users.noreply.github.com>
2023-06-20 20:44:48 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,8 +1,8 @@
 class Solution {
-    public int solution(int num) {
+    public int solution(long num) {
         int answer = 0;
         
-         while (answer != 480) {
+         while (answer != 500) {
             if (num != 1) {
                 if (num % 2 == 0) {
                     num = num / 2;
@@ -16,7 +16,7 @@ class Solution {
             }
         
         }
-        if (answer >=480  && num!=1){
+        if (answer >=500  && num!=1){
             answer = -1;
         }
         return answer;
