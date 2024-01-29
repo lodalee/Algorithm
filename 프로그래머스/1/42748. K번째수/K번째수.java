c935e61c660a625737e0adedde33516b37ce37d7
lodalee <131640279+lodalee@users.noreply.github.com>
2024-01-29 13:02:51 +0900

    [level 1] Title: K번째수, Time: 0.36 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -6,10 +6,10 @@ class Solution {
 
         for (int i = 0; i < commands.length; i++) {
             int start = commands[i][0] -1;
-            int end = commands[i][1] -1;
+            int end = commands[i][1];
             int target = commands[i][2] -1;
 
-            int[] slicedArr = Arrays.copyOfRange(array, start, end+1 );
+            int[] slicedArr = Arrays.copyOfRange(array, start, end);
 
             if (slicedArr.length == 1){
                 answer[i] = slicedArr[0];
