77ec1113c4585e7b7a2dd0d728798471e5e10009
lodalee <131640279+lodalee@users.noreply.github.com>
2024-01-29 15:21:44 +0900

    [level 0] Title: 옹알이 (1), Time: 0.21 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+
+        // 네 가지 발음
+        String[] 옹알이 = {"aya", "ye", "woo", "ma"};
+
+        for (int i = 0; i < babbling.length; i++) {
+            String temp = babbling[i];
+            for (int j = 0; j < 옹알이.length; j++) {
+                temp = temp.replace(옹알이[j], " ");
+            }
+            temp = temp.replace(" ","");
+            if (temp.length() == 0){
+                answer++;
+            }
+        }
+
+
+        return answer;
+    }
+}
