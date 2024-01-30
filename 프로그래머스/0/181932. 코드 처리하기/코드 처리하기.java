5fa9ac137e7a24922fefcb1ac6b2ba770acbd3f1
lodalee <131640279+lodalee@users.noreply.github.com>
2024-01-30 14:04:27 +0900

    [level 0] Title: 코드 처리하기, Time: 0.03 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -1,32 +1,21 @@
 class Solution {
     public String solution(String code) {
         boolean mood = false;
-        StringBuilder ret = new StringBuilder();
-        String[] strArr = code.split("");
+        StringBuilder sb = new StringBuilder();
 
-        //code for 문으로 순회
         for (int i = 0; i < code.length(); i++) {
-            //code[i]가 0이면 mood == false; /code[i] 가 1이면 mood = true;
-            if (strArr[i].equals("1")){
+            char ch = code.charAt(i);
+            if (ch == '1'){
                 mood = !mood;
+                continue;
             }
 
-            if (!(strArr[i].equals("1"))){
-                // (false) 짝수면 ret.append(code[i]);
-                if (mood == false && i % 2 == 0){
-                    ret.append(strArr[i]);
-                // (true) 홀수면 ret.append(code[i]);
-                } else if (mood == true && i % 2 == 1) {
-                    ret.append(strArr[i]);
-                }
+            if (mood == false && i % 2 == 0){
+                sb.append(ch);
+            } else if (mood == true && i % 2 == 1){
+                sb.append(ch);
             }
         }
-
-        String answer = "";
-        if (ret.length() == 0){
-            return "EMPTY";
-        } else {
-           return ret.toString();
-        }
+        return sb.length() == 0? "EMPTY" : sb.toString();
     }
 }
